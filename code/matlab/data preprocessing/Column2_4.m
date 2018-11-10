1ad5016c0c444bc26a291c6517f44a97bdd70397
xihajun <www.junfan.com@gmail.com>
2018-11-10 18:17:33 +0000

    Update Column2_4.m

--- code/matlab/data preprocessing/Column2_4.m
+++ code/matlab/data preprocessing/Column2_4.m
@@ -1,4 +1,5 @@
 %kddcup contains the data: src_bytes, dst_bytes, land
+%Preprocessing the 3 columns
 for j = 1:3
     A = unique(kddcup(:,j));
     for i =1:length(A)
@@ -6,10 +7,14 @@ for j = 1:3
         kddcup(f,j) = i;
     end
 end
+
+%Transfer str array to matrix
 temp = cellstr(kddcup);
 temp = char(temp);
 %str2num for the colunm 2 - 4
 K_24 = str2num(temp);
+%Reshape it as part 1
 Part1ofWholeK = reshape(K_24,length(kddcup),3);
 
+%Combine all the data sets
 WholeK=[Part1ofWholeK data label];
