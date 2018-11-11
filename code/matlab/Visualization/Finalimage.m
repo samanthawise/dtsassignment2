bd87dd8f06b4286bd724a4a63b6a357872f39aa2
xihajun <www.junfan.com@gmail.com>
2018-11-11 22:58:42 +0000

    Update Finalimage.m

--- code/matlab/Visualization/Finalimage.m
+++ code/matlab/Visualization/Finalimage.m
@@ -13,3 +13,11 @@ title("Test result")
 subplot(2,2,4)
 gscatter(mappedX(:,1), mappedX(:,2),(K_test(:,42)==1));
 title("The real result")
+%print
+get(gca);
+gaf=figure(1);
+scrsz=get(0,'ScreenSize');
+set(gaf,'Position',scrsz);
+print -djpeg '\\ads.bris.ac.uk\filestore\myfiles\StudentPG1\mw18386\Downloads\Data Science\visualize\400000\results.jpg' -r800
+print -dbitmap '\\ads.bris.ac.uk\filestore\myfiles\StudentPG1\mw18386\Downloads\Data Science\visualize\400000\results.bmp' -r800
+
